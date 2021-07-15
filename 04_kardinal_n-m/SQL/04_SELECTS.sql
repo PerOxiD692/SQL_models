d45212a0f73776f76171b6559a4a0e095d782658
Jan-Daniel Martin <jan-daniel.martin@gmx.de>
2021-07-15 11:36:35 +0200

    Update 04_SELECTS.sql

--- 04_kardinal_n-m/SQL/04_SELECTS.sql
+++ 04_kardinal_n-m/SQL/04_SELECTS.sql
@@ -20,6 +20,42 @@ SELECT
 FROM purchases
 INNER JOIN servants ON servants.id = purchases.servants_id
 INNER JOIN products ON products.id = purchases.products_id
-# WHERE servant_name ="Dieter"; 
-WHERE servants_id = 2;
+WHERE servant_name ="Dieter";
 
+-- Wieviele Produkte hat Dieter gekauft?
+SELECT 
+	CONCAT(servant_name, " kauft ", COUNT(product_name), " Produkte.") AS "Kaufhandlung"
+FROM purchases
+INNER JOIN servants ON servants.id = purchases.servants_id
+INNER JOIN products ON products.id = purchases.products_id
+WHERE servant_name ="Dieter"; 
+
+-- Wie viel Geld hat Dieter ausgegeben?
+SELECT 
+	CONCAT(servant_name, " gibt ", SUM(product_price), " € für seinen Einkauf aus.") AS "Kaufhandlung"
+FROM purchases
+INNER JOIN servants ON servants.id = purchases.servants_id
+INNER JOIN products ON products.id = purchases.products_id
+WHERE servant_name ="Dieter"; 
+
+-- Kombi Aggregiert / Nicht Aggregiert
+SELECT
+	servant_name AS "Diener",
+    sum(product_price) AS "Gesamtkosten"
+FROM purchases
+INNER JOIN servants ON servants.id = purchases.servants_id
+INNER JOIN products ON products.id = purchases.products_id
+GROUP BY servant_name # wichtig bei Aggregation!
+HAVING servant_name = "Dieter";
+
+
+-- Wer hat das Produkt X gekauft?
+-- Irgendwas mit Lachs / Irgendwas mit Sauce (LIKE)
+-- Spalten: Diener und Produkt
+SELECT
+	servant_name AS "DIENER",
+    product_name AS "Produkte mit Soße oder Lachs"
+FROM purchases
+INNER JOIN servants ON servants.id = purchases.servants_id
+INNER JOIN products ON products.id = purchases.products_id
+WHERE product_name LIKE "%Lachs%" OR product_name LIKE "%Sauce%";
